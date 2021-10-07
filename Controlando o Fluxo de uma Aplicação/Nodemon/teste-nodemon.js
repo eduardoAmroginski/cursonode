6ee31bca1713d79680b532c696db1f9c6a1c4f4b
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2021-10-07 16:07:25 -0300

    Adicionando exemplo de uso com nodemon

---
+++ Controlando o Fluxo de uma Aplicação/Nodemon/teste-nodemon.js
@@ -0,0 +1,6 @@
+const carro = "Ferrari"; //Executar o código usando nodemon e mudar o nome do carro.
+
+console.log(`Carro: ${carro}`);
+
+//Com o nodemon, sempre que o utilizamos para executar nosso servidor,
+//faz com que atualize automaticamente o servidor com o código novo.
